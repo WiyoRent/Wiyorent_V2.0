c16343259e1118dd54192f7396b3615a26bf85d4
PapiWinnie <w.nji1@alustudent.com>
2026-02-18 14:40:44 +0200

    feat(api): add Cloudinary configuration, database connection, and file upload middleware

---
+++ api/config/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+import "dotenv/config"
+
+export const connectCloudinary = () => {
+    try {
+        cloudinary.config({ 
+            cloud_name: process.env.CLOUDINARY_NAME, 
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET_KEY
+        });
+        console.log('Cloudinary Connected')
+    } catch (error) {
+        console.erjror(error)
+        throw error
+    }
+}
+
+
+
---
+++ api/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import 'dotenv/config'
+import { connectCloudinary } from './config/cloudinary.js'
+
+import cors from "cors"
+import adminRouter from './routes/admin/admin.routes.js'
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectCloudinary()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// Endpoints
+app.get('/', (req,res) => {
+    res.send("Welcom to WiyoBackend")
+})
+
+app.use('/api/v1/admin', adminRouter)
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`)
+})
