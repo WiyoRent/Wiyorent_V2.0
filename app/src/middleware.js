85938151df9297b3f5cf679d1abb1f6d6adadd76
PapiWinnie <w.nji1@alustudent.com>
2026-02-25 08:57:49 +0200

    feat(middleware): implement authentication middleware for protected routes

---
+++ app/src/middleware.js
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { auth } from "./auth";
+
+export async function middleware (req){
+    const session = await auth()
+    const user = session?.user
+    const {pathname} = req.nextUrl
+
+    // Protected Admin Paths -- NOT ADMIN
+    const adminProtectRoutes = ['/admin']
+    const role = user?.role
+    const isAdminRoute = adminProtectRoutes.some(path => pathname.startsWith(path))
+
+    if(isAdminRoute && role !== 'admin'){
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    // Protected User Paths --- NOT LOGIN
+    const protectedUserPaths = ['/housemates', '/profile']
+    const isProtectedUserPath = protectedUserPaths.some(path => pathname.startsWith(path))
+
+    if(isProtectedUserPath && !user){
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    // Protected housemate path for non-boarded user
+    // const protectedOnboardPath = ['/housemates']
+    // const isProtectedOnBoardPath = protectedOnboardPath.some(path => pathname.startsWith(path))
+    // if(isProtectedOnBoardPath && !user?.is_onboarded){
+    //     return NextResponse.redirect(new URL('/profile', req.url))
+    // }
+
+    // Protected HouseMate Path
+    const isVerified = user?.is_verified
+    if(pathname.startsWith('/housemates/') && !isVerified){
+        return NextResponse.redirect(new URL('/housemates', req.url))
+    }
+
+    return NextResponse.next()
+
+}
+
+export const config = {
+    match : ['/', '/housemates/:path*', '/profile', '/admin/:path*']
+}
+
+
