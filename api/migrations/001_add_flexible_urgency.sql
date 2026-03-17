28f8110f6a1b40d6126813404f8ac318c84483c3
PapiWinnie <w.nji1@alustudent.com>
2026-03-17 21:52:46 +0200

    feat (add) : Added wiyorent app icon

--- api/migrations/001_add_flexible_urgency.sql
+++
@@ -1,9 +0,0 @@
--- Migration: add 'flexible' to the urgency check constraint on users
--- Run once against your PostgreSQL database.
-
-ALTER TABLE users
-  DROP CONSTRAINT IF EXISTS users_urgency_check;
-
-ALTER TABLE users
-  ADD CONSTRAINT users_urgency_check
-  CHECK (urgency IN ('not_urgent', 'slightly_urgent', 'extremely_urgent', 'flexible'));
